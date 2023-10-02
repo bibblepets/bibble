f5bb96c8285992a43a22744fc089de0f46d723a5
tituswe <tituslowesg@gmail.com>
2023-10-03 00:27:27 +0800

    Add summary ui

--- client/src/modules/listing/Biology.tsx
+++ client/src/modules/listing/Biology.tsx
@@ -5,29 +5,6 @@ import ListingStage from '../../components/listing/listing/ListingStage';
 import ListingLayout from '../../layouts/ListingLayout';
 
 const Biology = () => {
-  const species = [
-    {
-      type: 'dog',
-      icon: GiSittingDog
-    },
-    {
-      type: 'cat',
-      icon: GiCat
-    },
-    {
-      type: 'rabbit',
-      icon: GiRabbit
-    },
-    {
-      type: 'mouse',
-      icon: GiRat
-    },
-    {
-      type: 'bird',
-      icon: GiHummingbird
-    }
-  ];
-
   return (
     <ListingLayout>
       <ListingStage title="Biology" subtitle="What's their breed and species?">
