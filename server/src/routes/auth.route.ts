a00d8edd716fb97af5306290dc8c8b3b96d3b59c
Tan Yan-Hao Joshua <joshuatanyanhao@gmail.com>
2023-10-10 20:09:33 +0800

    Add JSDocs to auth router

--- server/src/routes/auth.route.ts
+++ server/src/routes/auth.route.ts
@@ -3,10 +3,39 @@ import * as AuthController from '../controllers/auth.controller';
 
 const router = Router();
 
+/**
+ * @route GET /api/auth/status
+ * @desc Check if user is logged in
+ * @access Public
+ */
 router.get('/status', AuthController.checkAuthStatus);
+
+/**
+ * @route POST /api/auth/register
+ * @desc Register a new user
+ * @access Public
+ */
 router.post('/register', AuthController.registerUser);
+
+/**
+ * @route POST /api/auth/login
+ * @desc Login an existing user
+ * @access Public
+ */
 router.post('/login', AuthController.loginUser);
+
+/**
+ * @route POST /api/auth/logout
+ * @desc Logout an existing user
+ * @access Private
+ */
 router.post('/logout', AuthController.logoutUser);
+
+/**
+ * @route PUT /api/auth/update/:id
+ * @desc Update an existing user
+ * @access Private
+ */
 router.put('/update/:id', AuthController.updateUser);
 
 module.exports = router;
