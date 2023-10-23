956d87f6efba57bdc9678f02026ad6d55a88d4bc
Tan Yan-Hao Joshua <joshuatanyanhao@gmail.com>
2023-10-23 18:07:57 +0800

    Add Unit tests for User model

--- server/test/user/user.test.ts
+++ server/test/user/user.test.ts
@@ -236,9 +236,266 @@ describe('User model (CREATE)', () => {
 });
 
 describe('User model (READ)', () => {
+  const existingBuyerProfileData: ICreateBuyerProfileRequest['body'] = {
+    firstName: 'John',
+    lastName: 'Doe'
+  };
+
+  const existingUserData: Omit<ICreateUserRequest['body'], 'buyerProfile'> = {
+    email: 'test@example.com',
+    password: 'password'
+  };
+
+  let existingUserId: Schema.Types.ObjectId;
+
+  beforeEach(async function () {
+    const buyerProfile = new BuyerProfile(existingBuyerProfileData);
+    const savedBuyerProfile = await buyerProfile.save();
+
+    const user = new User({
+      ...existingUserData,
+      buyerProfile: savedBuyerProfile
+    });
+    const savedUser = await user.save();
+
+    existingUserId = savedUser._id;
+  });
+
   afterEach(async function () {
     await User.deleteMany({});
   });
+
+  it('+ Get User (by `id`)', async function () {
+    const user = await User.findById(existingUserId);
+
+    chai.expect(user).to.exist;
+    chai.expect(user?._id.toString()).to.be.equal(existingUserId.toString());
+  });
+
+  it('+ Get User (by `email`)', async function () {
+    const user = await User.findOne({ email: existingUserData.email });
+
+    chai.expect(user).to.exist;
+    chai.expect(user?._id.toString()).to.be.equal(existingUserId.toString());
+  });
+
+  it('- Get User (invalid `id`)', async function () {
+    try {
+      await User.findById('Invalid');
+    } catch (error: any) {
+      chai.expect(error.name).to.equal('CastError');
+    }
+  });
+
+  it('- Get User (invalid `email`)', async function () {
+    try {
+      await User.findOne({ email: 'Invalid' });
+    } catch (error: any) {
+      chai.expect(error.name).to.equal('CastError');
+    }
+  });
+
+  it('- Get User (`email` that does not exist)', async function () {
+    const user = await User.findOne({ email: 'missing@example.com' });
+
+    chai.expect(user).to.not.exist;
+  });
 });
 
-describe('User model (UPDATE)', () => {});
+describe('User model (UPDATE)', () => {
+  const existingBuyerProfileData: ICreateBuyerProfileRequest['body'] = {
+    firstName: 'John',
+    lastName: 'Doe'
+  };
+
+  const existingUserData: Omit<ICreateUserRequest['body'], 'buyerProfile'> = {
+    email: 'test@example.com',
+    password: 'password'
+  };
+
+  let existingUserId: Schema.Types.ObjectId;
+
+  beforeEach(async function () {
+    const buyerProfile = new BuyerProfile(existingBuyerProfileData);
+    const savedBuyerProfile = await buyerProfile.save();
+
+    const user = new User({
+      ...existingUserData,
+      buyerProfile: savedBuyerProfile
+    });
+    const savedUser = await user.save();
+
+    existingUserId = savedUser._id;
+  });
+
+  afterEach(async function () {
+    await User.deleteMany({});
+  });
+
+  it('+ Update User (only `email`)', async function () {
+    const updateUserData: IUpdateUserRequest['body'] = {
+      email: 'updated@example.com'
+    };
+
+    const updatedUser = await User.findOneAndUpdate(
+      { _id: existingUserId },
+      updateUserData,
+      { new: true }
+    );
+
+    chai.expect(updatedUser).to.exist;
+    chai
+      .expect(updatedUser?._id.toString())
+      .to.be.equal(existingUserId.toString());
+    chai.expect(updatedUser?.email).to.equal(updateUserData.email);
+  });
+
+  it('+ Update User (only `password`)', async function () {
+    const updateUserData: IUpdateUserRequest['body'] = {
+      password: 'updatedPassword'
+    };
+
+    const updatedUser = await User.findOneAndUpdate(
+      { _id: existingUserId },
+      updateUserData,
+      { new: true }
+    );
+
+    chai.expect(updatedUser).to.exist;
+    chai
+      .expect(updatedUser?._id.toString())
+      .to.be.equal(existingUserId.toString());
+    chai.expect(updatedUser?.password).to.not.equal(updateUserData.password);
+    chai.expect(updatedUser?.password).to.not.equal(existingUserData.password);
+    chai.expect(updatedUser?.isCorrectPassword(existingUserData.password)).to.be
+      .false;
+    chai.expect(updatedUser?.isCorrectPassword(updateUserData.password!)).to.be
+      .true;
+  });
+
+  it('+ Update User (all fields excluding profiles)', async function () {
+    const updateUserData: IUpdateUserRequest['body'] = {
+      email: 'updated@example.com',
+      password: 'updatedPassword'
+    };
+
+    const updatedUser = await User.findOneAndUpdate(
+      { _id: existingUserId },
+      updateUserData,
+      { new: true }
+    );
+
+    chai.expect(updatedUser).to.exist;
+    chai
+      .expect(updatedUser?._id.toString())
+      .to.be.equal(existingUserId.toString());
+    chai.expect(updatedUser?.email).to.equal(updateUserData.email);
+    chai.expect(updatedUser?.password).to.not.equal(updateUserData.password);
+    chai.expect(updatedUser?.password).to.not.equal(existingUserData.password);
+    chai.expect(updatedUser?.isCorrectPassword(existingUserData.password)).to.be
+      .false;
+    chai.expect(updatedUser?.isCorrectPassword(updateUserData.password!)).to.be
+      .true;
+  });
+
+  it('- Update User (invalid `email`)', async function () {
+    try {
+      const updateUserData: IUpdateUserRequest['body'] = {
+        email: 'updated'
+      };
+
+      await User.findOneAndUpdate({ _id: existingUserId }, updateUserData, {
+        new: true
+      });
+    } catch (error: any) {
+      chai.expect(error.name).to.equal('ValidationError');
+    }
+  });
+
+  it('- Update User (invalid `password`)', async function () {
+    try {
+      const updateUserData: IUpdateUserRequest['body'] = {
+        password: 'pass'
+      };
+
+      await User.findOneAndUpdate({ _id: existingUserId }, updateUserData, {
+        new: true
+      });
+    } catch (error: any) {
+      chai.expect(error.name).to.equal('ValidationError');
+    }
+  });
+});
+
+describe('User model (DELETE)', () => {
+  const existingBuyerProfileData: ICreateBuyerProfileRequest['body'] = {
+    firstName: 'John',
+    lastName: 'Doe'
+  };
+
+  const existingUserData: Omit<ICreateUserRequest['body'], 'buyerProfile'> = {
+    email: 'test@example.com',
+    password: 'password'
+  };
+
+  let existingUserId: Schema.Types.ObjectId;
+
+  beforeEach(async function () {
+    const buyerProfile = new BuyerProfile(existingBuyerProfileData);
+    const savedBuyerProfile = await buyerProfile.save();
+
+    const user = new User({
+      ...existingUserData,
+      buyerProfile: savedBuyerProfile
+    });
+    const savedUser = await user.save();
+
+    existingUserId = savedUser._id;
+  });
+
+  afterEach(async function () {
+    await User.deleteMany({});
+  });
+
+  it('+ Delete User (by `id`)', async function () {
+    const deletedUser = await User.findByIdAndDelete(existingUserId);
+
+    chai.expect(deletedUser).to.exist;
+    chai
+      .expect(deletedUser?._id.toString())
+      .to.be.equal(existingUserId.toString());
+  });
+
+  it('+ Delete User (by `email`)', async function () {
+    const deletedUser = await User.findOneAndDelete({
+      email: existingUserData.email
+    });
+
+    chai.expect(deletedUser).to.exist;
+    chai
+      .expect(deletedUser?._id.toString())
+      .to.be.equal(existingUserId.toString());
+  });
+
+  it('- Delete User (invalid `id`)', async function () {
+    try {
+      await User.findByIdAndDelete('Invalid');
+    } catch (error: any) {
+      chai.expect(error.name).to.equal('CastError');
+    }
+  });
+
+  it('- Delete User (invalid `email`)', async function () {
+    try {
+      await User.findOneAndDelete({ email: 'Invalid' });
+    } catch (error: any) {
+      chai.expect(error.name).to.equal('CastError');
+    }
+  });
+
+  it('- Delete User (`email` that does not exist)', async function () {
+    const user = await User.findOneAndDelete({ email: 'missing@example.com' });
+
+    chai.expect(user).to.not.exist;
+  });
+});
