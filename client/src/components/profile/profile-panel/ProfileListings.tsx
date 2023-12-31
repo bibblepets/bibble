4bac35cefa6a0d36a0f05d1467b52898aaf77688
tituswe <tituslowesg@gmail.com>
2023-12-31 20:33:55 +0800

    Change show all listings functionality

--- client/src/components/profile/profile-panel/ProfileListings.tsx
+++ client/src/components/profile/profile-panel/ProfileListings.tsx
@@ -1,14 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useSelector } from 'react-redux';
 import { selectCurrentUser } from '../../../features/authSlice';
 import { selectMyListings } from '../../../features/listingSlice';
 import ProfileListingCard from './ProfileListingCard';
-import { useNavigate } from 'react-router-dom';
 
 const ProfileListings = () => {
-  const navigate = useNavigate();
   const currentUser = useSelector(selectCurrentUser);
   const myListings = useSelector(selectMyListings);
+  const [isShowingAll, setIsShowingAll] = useState(false);
+  const shownListings = isShowingAll ? myListings : myListings.slice(0, 4);
 
   return (
     <div className="flex flex-col gap-8">
@@ -16,16 +16,18 @@ const ProfileListings = () => {
         Listings posted by {currentUser?.businessProfile?.businessName}
       </h2>
       <div className="grid grid-cols-2 gap-4">
-        {myListings.slice(0, 4).map((listing, index) => (
+        {shownListings.map((listing, index) => (
           <ProfileListingCard key={index} listing={listing} />
         ))}
       </div>
       <div>
         <button
-          onClick={() => navigate('/listing')}
+          onClick={() => setIsShowingAll(!isShowingAll)}
           className="border border-gray-600 transition hover:bg-gray-200 rounded-lg py-2 px-4 font-semibold text-sm"
         >
-          Show all {myListings.length} listings
+          {isShowingAll
+            ? 'Show less'
+            : `Show all ${myListings.length} listings`}
         </button>
       </div>
     </div>
