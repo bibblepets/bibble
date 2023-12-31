de348f800df6e395db44872057e3b2ec100eadb5
tituswe <tituslowesg@gmail.com>
2023-12-31 20:30:14 +0800

    Fix cursor pointer bug:

--- client/src/components/profile/profile-panel/ProfileListingCard.tsx
+++ client/src/components/profile/profile-panel/ProfileListingCard.tsx
@@ -15,21 +15,23 @@ const ProfileListingCard: React.FC<ProfileListingCardProps> = ({ listing }) => {
       onClick={() => navigate(`/details/${listing._id}`)}
       className="flex flex-row p-4 border rounded-xl gap-8 items-center cursor-pointer"
     >
-      <div className="flex flex-row gap-2">
+      <div className="flex flex-row gap-2 cursor-pointer">
         <img
           className="rounded-lg w-[96px] h-[96px] object-cover"
           src={listing.media[0] ? listing.media[0].url : placeholderPup}
         />
       </div>
-      <div className="flex flex-col gap-2">
-        <label className="text-gray-800">
+      <div className="flex flex-col gap-2 cursor-pointer">
+        <label className="text-gray-800 cursor-pointer">
           {listing.animal.breeds.map((breed) => breed.name).join(', ')}{' '}
-          <span className="font-light text-gray-500">{listing.species}</span>
+          <span className="font-light text-gray-500 cursor-pointer">
+            {listing.species}
+          </span>
         </label>
-        <label className="font-light text-gray-500">
+        <label className="font-light text-gray-500 cursor-pointer">
           {listing.animal.gender}
         </label>
-        <label className="font-light text-gray-500">
+        <label className="font-light text-gray-500 cursor-pointer">
           {listing.animal.origin.name}
         </label>
       </div>
